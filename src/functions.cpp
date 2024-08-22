fe7bb3165dbf27acc7f83c91c15064d21652f009
Crasto <Alastair.Crasto@wattswater.com>
2024-08-22 13:56:43 +1200

    Function comments

--- src/functions.cpp
+++ src/functions.cpp
@@ -3,11 +3,11 @@
 
 float pressureSensor() {
   float pressure = 0;
-  float sensorValue = analogRead(A0);  //reading anaolgue value at pin A0
-  // pressure = 1.1769 * sensorValue + 5.4893;  // analogue reading to kPA conversion
-  pressure = 1.1843 * sensorValue - 2.5289;
+  float sensorValue = analogRead(A0);         //reading anaolgue value at pin A0
 
-  if (pressure < 6) {  // too low of a pressure to capture - assuming 0 due to conversion error
+  pressure = 1.1843 * sensorValue - 2.5289;   // Linear relationship between the analog values and output pressure
+
+  if (pressure < 6) {                         // too low of a pressure to capture - assuming 0 due to conversion error
     return 0;
   } else {
     return pressure;
@@ -20,7 +20,7 @@ float inletPressureSensor() {
   float sensorValue = analogRead(A1);
   pressureSend = sensorValue;
 
-  if (sensorValue < 1) {  // too low of a pressure to capture - assuming 0 due to conversion error
+  if (sensorValue < 1) {                     // too low of a pressure to capture - assuming 0 due to conversion error
     return 0;
   } else {
     return pressureSend;
@@ -31,13 +31,13 @@ void pressureAdjustment(float errorValue,int springDirPin,int springStepPin,int 
   Serial.println(" Entered automatic adjustment");
   Serial.print("errorValue : ");
   Serial.println(errorValue);
-  if (errorValue > 0) {  // Setting the direction for the motor
+  if (errorValue > 0) {                     // Setting the direction for the motor (Current output pressure is greater than the setpoint)
     digitalWrite(springDirPin, LOW);
     digitalWrite(input, LOW);
     digitalWrite(output, HIGH);
     delay(200);
-    Serial.println("Reversal");
-    for (int i = 0; i <= 800; i++) {
+    Serial.println("Reversal");             // unwinds the spring housing for two rotations 
+    for (int i = 0; i <= 800; i++) {        // 800 value is based on the step/rev setting 
       digitalWrite(springStepPin, HIGH);
       delayMicroseconds(500);
       digitalWrite(springStepPin, LOW);
@@ -45,22 +45,22 @@ void pressureAdjustment(float errorValue,int springDirPin,int springStepPin,int 
     }
     delay(500);
   } else {
-    Serial.println("Tightening");
+    Serial.println("Tightening");           // if the error value is less than 0 (Current output is lower than the setpoint)
     digitalWrite(springDirPin, HIGH);
-    while (errorValue < -1) {
+
+    while (errorValue < -1) {               // keeps winding the driver until the error is under the value of 1
       digitalWrite(springStepPin, HIGH);
       delayMicroseconds(500);
       digitalWrite(springStepPin, LOW);
       delayMicroseconds(500);
 
-      float pressure_check = pressureSensor();
-      errorValue = pressure_check - setpoint;
+      float pressure_check = pressureSensor(); 
+      errorValue = pressure_check - setpoint;   //checking output pressure and comparing to error to see if condition of being within 1 is met
     }
   }
 
   digitalWrite(input, HIGH);
   digitalWrite(output, LOW);
-  //digitalWrite(springCylinder,HIGH);
   delay(500);
 
   Serial.println("Exiting adjustment --> back to loop function");
@@ -75,7 +75,7 @@ bool shutOffFunction(int spindleDirPin,int spindleStepPin,int input, int output)
     digitalWrite(input, LOW);
     digitalWrite(output, HIGH);
     delay(600);
-    for (int j = 0; j < 9; j++) {
+    for (int j = 0; j < 9; j++) {                  // driver winds the spindle down. Value of 9 based on multiple tests to see number of rotations needed to complete shut off 
       for (int i = 0; i <= 400; i++) {
         digitalWrite(spindleStepPin, HIGH);
         delayMicroseconds(500);
@@ -87,7 +87,8 @@ bool shutOffFunction(int spindleDirPin,int spindleStepPin,int input, int output)
     coarse_tighten = false;
   }
 
-  //digitalWrite(outputRelay,LOW);
+  // The following code tests the functionality of the spindle to check for any pressure creep despite shut off
+
   digitalWrite(input, HIGH);
   digitalWrite(output, LOW);
   delay(500);
@@ -125,12 +126,12 @@ bool pressureDecay(int input, int output) {
   digitalWrite(output, LOW);
   delay(1000);
 
-  digitalWrite(input, LOW);
+  digitalWrite(input, LOW);                               // Valve is now isolated and checked if it holds pressure
 
-  pressureTest = inletPressureSensor();
+  pressureTest = inletPressureSensor();                   // inlet pressure value used to check for leaks in the valve
   Serial.print("System pressure before test: ");
   Serial.println(pressureTest);
-  delay(5000);
+  delay(5000);                                            
   nextSample = inletPressureSensor();
   Serial.print("System pressure after test: ");
   Serial.println(nextSample);
@@ -141,7 +142,7 @@ bool pressureDecay(int input, int output) {
   Serial.print("Pressure decay: ");
   Serial.println(abs(nextSample - pressureTest));
 
-  if (abs(nextSample - pressureTest) > 50) {
+  if (abs(nextSample - pressureTest) > 50) {              // Any leaks above 50 are termed as leaks through the valve 
     return false;
   } else {
     return true;
@@ -152,15 +153,15 @@ bool springPressureHold(int springDirPin,int springStepPin,int input,int output)
   float preSample = 0;
   float postSample = 0;
 
-  digitalWrite(input, HIGH);
+  digitalWrite(input, HIGH);                                          // pressure held through the valve
   digitalWrite(output, LOW);
   delay(500);
 
-  preSample = pressureSensor();
+  preSample = pressureSensor();                                       // outlet pressure value checked
   Serial.print("Pressure before pressure is adjusted : ");
   Serial.println(preSample);
 
-  digitalWrite(springDirPin, HIGH);
+  digitalWrite(springDirPin, HIGH);                                   // winding the spring housing to check if its functioning correctly
 
   for (int i = 0; i <= 800; i++) {
     digitalWrite(springStepPin, HIGH);
@@ -186,14 +187,14 @@ bool springPressure(float pressureSetpoint) {
   float postSample = 0;
 
   Serial.print("Pressure before delay to check for creep : ");
-  Serial.println(pressureSetpoint);
+  Serial.println(pressureSetpoint);                                   // pressureSetpoint variable is the mark at which the spring housing was set to
   delay(15000);
 
-  postSample = pressureSensor();
+  postSample = pressureSensor();                                      // postSample is to check for any pressure creep once the output pressure has been set
   Serial.print("Pressure after delay to check for creep : ");
   Serial.println(postSample);
 
-  if (abs(pressureSetpoint - postSample) > 15) {
+  if (abs(pressureSetpoint - postSample) > 15) {                    
     return false;
   } else {
     return true;
@@ -208,7 +209,7 @@ bool secondPulse(float currentPressure,int input,int output) {
   float newPressure = 0;
   bool holdingPressure = true;
 
-  while (pulseNumber < 3) {
+  while (pulseNumber < 3) {                                       // three total pulses in the whole operation
     digitalWrite(output, LOW);
     delay(1500);
     newPressure = pressureSensor();
@@ -216,10 +217,10 @@ bool secondPulse(float currentPressure,int input,int output) {
     Serial.print(pulseNumber + 1);
     Serial.print(" pulse : ");
     Serial.println(newPressure);
-    if (abs(currentPressure - newPressure) >= 20) {
+    if (abs(currentPressure - newPressure) >= 20) {               // if the pressure after a pulse is greater than 20. valve fails the pulse test
       holdingPressure = false;
       Serial.println("Pulsing failed");
-      return holdingPressure;
+      return holdingPressure;                                     // valve has passed the current leg of the pulsing operation
     }
     digitalWrite(output, HIGH);
     delay(500);
@@ -233,6 +234,7 @@ bool spindleReversal(int spindleDirPin,int spindleStepPin,int input, int output,
 
   int upCount = 0;
 
+  /*
   Serial.println("Loosening for leak test");
   digitalWrite(spindleDirPin, LOW);
   for (int i = 0; i <= 400; i++) {
@@ -241,21 +243,23 @@ bool spindleReversal(int spindleDirPin,int spindleStepPin,int input, int output,
     digitalWrite(spindleStepPin, LOW);
     delayMicroseconds(500);
   }
+  */
 
   digitalWrite(input, HIGH);
   digitalWrite(output, LOW);
 
   bool spindleLeakState = false;
 
-  while (spindleLeakState == false) {
+  while (spindleLeakState == false) {                           // operations stay within this loop until operator is not satisfied with the leak test
 
-    bool spindleUpState = digitalRead(spindleUpButton);
+    // buttons to control the wind/unwind of the spindle and pass/fail
+    bool spindleUpState = digitalRead(spindleUpButton);       
     bool spindleDownState = digitalRead(spindleDownButton);
     bool spindlePass = digitalRead(spindlePassButton);
     bool spindleFail = digitalRead(spindleFailButton);
 
 
-    if (spindleDownState == HIGH) {
+    if (spindleDownState == HIGH) {                             // drives the spindle down
       digitalWrite(spindleDirPin, HIGH);
       for (int i = 0; i <= 400; i++) {
         digitalWrite(spindleStepPin, HIGH);
@@ -267,7 +271,7 @@ bool spindleReversal(int spindleDirPin,int spindleStepPin,int input, int output,
       Serial.println(upCount);
     }
 
-    if (spindleUpState == HIGH) {
+    if (spindleUpState == HIGH) {                              // drives the spindle up
       digitalWrite(spindleDirPin, LOW);
       for (int i = 0; i <= 400; i++) {
         digitalWrite(spindleStepPin, HIGH);
@@ -275,16 +279,16 @@ bool spindleReversal(int spindleDirPin,int spindleStepPin,int input, int output,
         digitalWrite(spindleStepPin, LOW);
         delayMicroseconds(500);
       }
-      upCount++;
+      upCount++;                                               // variable keeps check of how many times the spindle was winded/unwinded
       Serial.println(upCount);
     }
 
-    if (spindleFail == HIGH) {
+    if (spindleFail == HIGH) {                                 // operator presses the fail button
       return false;
     }
 
     if (upCount >= 6) {
-      if (spindlePass == HIGH) {
+      if (spindlePass == HIGH) {                               // operator must check atleast 6 locations of unwinding before they can try pass the valve
         return true;
       }
     }
